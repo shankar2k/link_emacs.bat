8f47a8e51f02820107672af3a1019010819c0d5f
Shankar Rao <shankar.rao@gmail.com>
2024-01-26 09:51:32 +0100

    Rename project to link_emacs.bat

--- link_emacs.bat
+++ link_emacs.bat
@@ -1,4 +1,40 @@
 @echo off
+rem link_emacs.bat --- Fix path, context menu, and daemon links after Emacs upgrade
+rem
+rem Copyright (C) 2024 Shankar Rao
+rem
+rem
+rem * Commentary:
+rem
+rem This Windows batch script updates the PATH environment variable, commands
+rem in the context menu and the link to the Emacs daemon in Task Scheduler to
+rem refer to the latest installed version of Emacs.
+rem
+rem See documentation on https://github.com/shankar2k/link_emacs.bat/.
+rem
+rem * License:
+rem
+rem This program is free software; you can redistribute it and/or modify
+rem it under the terms of the GNU General Public License as published by
+rem the Free Software Foundation, either version 3 of the License, or
+rem (at your option) any later version.
+rem
+rem This program is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU General Public License for more details.
+rem
+rem You should have received a copy of the GNU General Public License
+rem along with this program.  If not, see <http://www.gnu.org/licenses/>.
+rem
+rem * History:
+rem
+rem Version 0.1 (2024-01-25):
+rem
+rem - Initial version
+rem
+rem * Code:
+
 setlocal enabledelayedexpansion
 set EMACSROOT=C:\Program Files\Emacs
 
